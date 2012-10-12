2331b584343637cfddd4147c1631c4c595a7c4b4
ekborra <erik@erikborra.net>
2012-10-12 14:00:32 +0200

    little clean up

--- common/CowordOnTools.class.php
+++ common/CowordOnTools.class.php
@@ -1,6 +1,7 @@
 <?php
+
 set_time_limit(0);
-ini_set('memory_limit','2G');
+ini_set('memory_limit', '2G');
 date_default_timezone_set('Europe/Amsterdam');
 include_once('Gexf.class.php');
 include_once('dmi_api/DmiTool.class.php');
@@ -8,66 +9,57 @@ include_once('dmi_api/DmiTool.class.php');
 // class allowing use of coword machine on tools.digitalmethods.net
 class CowordOnTools {
 
-	public $series;			// stores series of cowords, per day
-	public $scriptStartTime;
+    public $series;   // stores series of cowords, per day
+    public $scriptStartTime;
 
     function __construct() {
         $this->series = array();
         $this->time = 0;
     }
 
-    function getCowords($texts,$time) {
-    	$this->scriptStartTime = date('U');
-    	print "Time ".strftime("%T",$this->scriptStartTime)."<bR>"; 
-    	print "getting cowords<br>";
-    	$tool=new DmiTool('coword',DMI_TESTING);
-    	$tool->setParm('text_json',json_encode($texts));
-    	$tool->setParm('options[]','urls,remove_stopwords');
-		$tool->setParm('stopwordList','english');
-		$tool->setParm('max_document_frequency',90);
-		$tool->setParm('min_frequency',2);
-    //          'threshold_of_associations' => 0.2,
-    	$this->time = $time;
-		$tool->execute(array($this,"toolCallback"));
-//		print "<b>series:</b><br>";
-//print_r($this->series);
-		file_put_contents('/home/erik/cowords/climate-change_'.$this->time.".json",json_encode($this->series));
-	}
-		
-	function toolCallback($result) {
-		$cowordlist = array();
-		print "Time ".strftime("%T",date('U'))."<br>";
-		if(empty($result)) 
-			print "<b>Nothing found for $time</b><br/>";
-		else {
-			$cowordlist = $this->intoTuples($result);
-			print count($cowordlist)." tuples found<br>";
-		}
-		$this->series[$this->time] = $cowordlist;
-    }
-    
-    // as the response from the coword machine now is a tree, we now need to reconstruct the tuples
-    function intoTuples($results) {
-//print "<b>results:</b><br>";
-//print_r($results);
-    	$cowordlist = array();
-    	foreach($results as $word => $cowords) {
-    		foreach($cowords as $coword => $frequency) {
-    			$obj = new StdClass;
-    			$obj->word = $word;
-    			$obj->coword = $coword;
-    			$obj->frequency = $frequency;
-    			$cowordlist[] = $obj;
-    		}
-    	}
-//    	print "<br>";
-//    	print "<b>cowordlist</b><br>";
-//    	print_r($cowordlist);
-//    	print "<br>";
-    	return $cowordlist;
+    function getCowords($texts, $time) {
+        $this->scriptStartTime = date('U');
+        print "Time " . strftime("%T", $this->scriptStartTime) . "<bR>";
+        print "getting cowords<br>";
+        $tool = new DmiTool('coword', DMI_LOCAL);
+        $tool->setParm('text_json', json_encode($texts));
+        $tool->setParm('options[]', 'urls,remove_stopwords');
+        $tool->setParm('stopwordList', 'english');
+//        $tool->setParm('max_document_frequency', 90);
+        $tool->setParm('min_frequency', 2);
+        //          'threshold_of_associations' => 0.2,
+        $this->time = $time;
+        $tool->execute(array($this, "toolCallback"));
     }
 
-    function gexfTimeSeries($title,$word_frequencies = array()) {
+    function toolCallback($result) {
+        $cowordlist = array();
+        print "Time " . strftime("%T", date('U')) . "<br>";
+        if (empty($result))
+            print "<b>Nothing found for $time</b><br/>";
+        else {
+            $cowordlist = $this->intoTuples($result);
+            print count($cowordlist) . " tuples found<br>";
+        }
+        $this->series[$this->time] = $cowordlist;
+    }
+
+    // as the response from the coword machine now is a tree, we now need to reconstruct the tuples
+    function intoTuples($results) {
+        $cowordlist = array();
+        foreach ($results as $word => $cowords) {
+            foreach ($cowords as $coword => $frequency) {
+                $obj = new StdClass;
+                $obj->word = $word;
+                $obj->coword = $coword;
+                $obj->frequency = $frequency;
+                $cowordlist[] = $obj;
+            }
+        }
+        return $cowordlist;
+    }
+
+    function gexfTimeSeries($title, $word_frequencies = array()) {
         $gexf = new Gexf();
         $gexf->setTitle("Co-word " . $title);
         $gexf->setEdgeType(GEXF_EDGE_UNDIRECTED);
@@ -77,12 +69,12 @@ class CowordOnTools {
         // get words for each time slice
         $words = array();
         foreach ($this->series as $time => $cowordlist) {
-	        foreach($cowordlist as $tuple) {
-	        	$word = $tuple->word;
-	        	$coword = $tuple->coword;
-	        	$words[$word][$time] = 1;
-	            $words[$coword][$time] = 1;
-	        }
+            foreach ($cowordlist as $tuple) {
+                $word = $tuple->word;
+                $coword = $tuple->coword;
+                $words[$word][$time] = 1;
+                $words[$coword][$time] = 1;
+            }
         }
         // count nr of time slices the words appears in
         foreach ($words as $word => $times) {
@@ -95,24 +87,24 @@ class CowordOnTools {
 
         // make nodes
         foreach ($this->series as $time => $cowordlist) {
-            foreach ($cowordlist as $tuple) { 
-            	$word = $tuple->word;
-            	$coword = $tuple->coword;
-            	$frequency = $tuple->frequency;
-            	       
+            foreach ($cowordlist as $tuple) {
+                $word = $tuple->word;
+                $coword = $tuple->coword;
+                $frequency = $tuple->frequency;
+
                 $node1 = new GexfNode($word);
-                if(isset($word_frequencies[$word]))
-                	$node1->addNodeAttribute("word_frequency",$word_frequencies[$word],$type="int");
+                if (isset($word_frequencies[$word]))
+                    $node1->addNodeAttribute("word_frequency", $word_frequencies[$word], $type = "int");
                 $gexf->addNode($node1);
                 if ($documentsPerWords[$word] > $threshold)
                     $node1->setNodeColor(0, 255, 0, 0.75);
                 $gexf->nodeObjects[$node1->id]->addNodeSpell($time, $time);
                 $node2 = new GexfNode($coword);
-                if(isset($word_frequencies[$coword]))
-                	$node2->addNodeAttribute("word_frequency",$word_frequencies[$coword],$type="int");
+                if (isset($word_frequencies[$coword]))
+                    $node2->addNodeAttribute("word_frequency", $word_frequencies[$coword], $type = "int");
                 $gexf->addNode($node2);
                 if ($documentsPerWords[$coword] > $threshold)
-                    $node2->setNodeColor(0, 255, 0, 0.75);                    
+                    $node2->setNodeColor(0, 255, 0, 0.75);
                 $gexf->nodeObjects[$node2->id]->addNodeSpell($time, $time);
                 $edge_id = $gexf->addEdge($node1, $node2, $frequency);
                 $gexf->edgeObjects[$edge_id]->addEdgeSpell($time, $time);
@@ -121,6 +113,7 @@ class CowordOnTools {
         $gexf->render();
         return $gexf->gexfFile;
     }
+
 }
 
 ?>
