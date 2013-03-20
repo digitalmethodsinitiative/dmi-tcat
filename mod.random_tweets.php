c3dd791b59677a5b9431aec576960aac9e326ef7
erik <erik@digitalmethods.net>
2013-03-20 10:51:18 +0100

    added urls and hashtags to export

--- mod.random_tweets.php
+++ mod.random_tweets.php
@@ -46,38 +46,74 @@ require_once './common/functions.php';
 
         </table>
 
-<?php
-if ($samplesize > 0) {
+        <?php
+        if ($samplesize > 0) {
 
-    echo '<fieldset class="if_parameters">';
+            echo '<fieldset class="if_parameters">';
 
-    echo '<legend>Your File</legend>';
+            echo '<legend>Your File</legend>';
 
-    validate_all_variables();
-    $exc = (empty($esc['shell']["exclude"])) ? "" : "-" . $esc['shell']["exclude"];
-    $filename = $resultsdir . $esc['shell']["datasetname"] . "_" . $esc['shell']["query"] . $exc . "_" . $esc['date']["startdate"] . "_" . $esc['date']["enddate"] . "_" . $esc['shell']["from_user_name"] . "_" . $samplesize . "randomTweets.csv";
+            validate_all_variables();
+            $exc = (empty($esc['shell']["exclude"])) ? "" : "-" . $esc['shell']["exclude"];
+            $filename = $resultsdir . $esc['shell']["datasetname"] . "_" . $esc['shell']["query"] . $exc . "_" . $esc['date']["startdate"] . "_" . $esc['date']["enddate"] . "_" . $esc['shell']["from_user_name"] . "_" . $samplesize . "randomTweets.csv";
 
-    if (1 || !file_exists($filename)) {
+            if (1 || !file_exists($filename)) {
 
-        $content = "time,created_at,from_user_name,text,source,location,lat,lng\n";
+                $header = "time,created_at,from_user_name,text,source,location,lat,lng,hashtags,urls\n";
 
-        $sql = "SELECT * FROM " . $esc['mysql']['dataset'] . "_tweets t WHERE ";
-        $sql .= sqlSubset();
-        $sql .= "ORDER BY RAND() LIMIT " . $samplesize;
+                $sql = "SELECT * FROM " . $esc['mysql']['dataset'] . "_tweets t WHERE ";
+                $sql .= sqlSubset();
+                $sql .= "ORDER BY RAND() LIMIT " . $samplesize;
 
-        $sqlresults = mysql_query($sql);
-        while ($data = mysql_fetch_assoc($sqlresults)) {
-            $content .= strtotime($data["created_at"]) . "," . $data["created_at"] . "," . $data["from_user_name"] . "," . validate($data["text"], "tweet") . ",\"" . strip_tags(html_entity_decode($data["source"])). "\",\"" . trim(strip_tags(html_entity_decode($data["location"]))). "\",".$data['geo_lat'].",".$data['geo_lng']."\n"; // @todo, add stuff like location // @todo character encoding
+                $sqlresults = mysql_query($sql);
+                $content = array();
+                while ($data = mysql_fetch_assoc($sqlresults)) {
+                    $content[$data['id']] = strtotime($data["created_at"]) . "," . $data["created_at"] . "," . $data["from_user_name"] . "," . validate($data["text"], "tweet") . ",\"" . strip_tags(html_entity_decode($data["source"])) . "\",\"" . preg_replace("/[\r\t\n,]/"," ",trim(strip_tags(html_entity_decode($data["location"])))) . "\"," . $data['geo_lat'] . "," . $data['geo_lng'];
+                }
+
+                // get hashtags
+                $sql = "SELECT * FROM " . $esc['mysql']['dataset'] . "_hashtags WHERE ";
+                $sql .= "tweet_id IN (" . implode(",", array_keys($content)) . ")";
+                $sqlresults = mysql_query($sql);
+                while ($data = mysql_fetch_assoc($sqlresults)) {
+                    $hashtags[$data['tweet_id']][] = $data['text'];
+                }
+
+                // get urls
+                $sql = "SELECT tweet_id,url_followed FROM " . $esc['mysql']['dataset'] . "_urls WHERE ";
+                $sql .= "tweet_id IN (" . implode(",", array_keys($content)) . ")";
+                //print $sql . "<br>";
+                $sqlresults = mysql_query($sql);
+                $urls = array();
+                while ($data = mysql_fetch_assoc($sqlresults)) {
+                    $urls[$data['tweet_id']][] = $data['url_followed'];
+                }
+
+                $out = $header;
+                foreach ($content as $id => $line) {
+                    $out .= $line;
+                    if (isset($hashtags[$id]))
+                        $out .= "," . implode(" ; ", $hashtags[$id]);
+                    else
+                        $out .= ",";
+                    if (isset($urls[$id]))
+                        $out .= "," . implode(" ; ", $urls[$id]);
+                    else
+                        $out .= ",";
+                    if (substr($out, 0, -1) == ",")
+                        $out = substr($out, 0, -1);
+                    //$out .= ",".$id;
+                    $out .= "\n";
+                }
+
+                file_put_contents($filename, chr(239) . chr(187) . chr(191) . $out);
+            }
+
+            echo '<p><a href="' . str_replace("#", urlencode("#"), str_replace("\"", "%22", $filename)) . '">' . $filename . '</a></p>';
+
+            echo '</fieldset>';
         }
-
-        file_put_contents($filename, chr(239) . chr(187) . chr(191) . $content);
-    }
-
-    echo '<p><a href="' . str_replace("#", urlencode("#"), str_replace("\"", "%22", $filename)) . '">' . $filename . '</a></p>';
-
-    echo '</fieldset>';
-}
-?>
+        ?>
 
     </body>
 </html>
