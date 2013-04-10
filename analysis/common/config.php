3173b9a96cc647efe54c4208131987243237de90
erik <erik@digitalmethods.net>
2013-04-10 10:36:30 +0200

    added config for analysis

---
+++ analysis/common/config.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once('../config.php');
+include_once('../querybins.php');
+
+/*
+ * Whether to cache results
+ */
+$cacheresults = false;
+
+/*
+ * Where to cache results
+ */
+$resultsdir = "cache/";
+
+?>
