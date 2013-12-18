e49c9b3962e4c4a92c0221d85ecf0e12468ce37a
Emile den Tex <emile@digitalmethods.net>
2013-12-18 16:27:42 +0100

    * Flush buffer when handling TERM signal (inserts tweets in db)

--- capture/common/termhandler.php
+++ capture/common/termhandler.php
@@ -14,11 +14,32 @@ function install_capture_signal_handlers() {
 
 }
 
+function capture_flush_buffer() {
+
+     global $tweetbucket;
+
+     logit(CAPTURE . ".error.log", "attempting to flush the tweets capture buffer");
+
+     if (is_array($tweetbucket) && is_callable('processtweets')) {
+
+          $copy = $tweetbucket;         // avoid any parallel processing by copy and then empty
+          $tweetbucket = array();
+
+          processtweets($copy);
+
+     }
+
+}
+
 function capture_signal_handler_term($signo) {
 
      global $exceeding, $ratelimit, $ex_start;
 
-     logit(CAPTURE . ".error.log", "received TERM signal, writing rate limit information to database");
+     logit(CAPTURE . ".error.log", "received TERM signal");
+
+     capture_flush_buffer();
+
+     logit(CAPTURE . ".error.log", "writing rate limit information to database");
 
      if (isset($exceeding) && $exceeding == 1) {
           ratelimit_record($ratelimit, $ex_start);
