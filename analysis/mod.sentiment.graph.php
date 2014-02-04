0ee11c963b806f572b1b0e950ded8b1c36ea9cd2
erik <erik@digitalmethods.net>
2014-02-04 13:23:20 +0100

    added subjective sentiment

--- analysis/mod.sentiment.graph.php
+++ analysis/mod.sentiment.graph.php
@@ -19,6 +19,8 @@ var sdata = new google.visualization.DataTable()
 sdata.addColumn('string', 'Date');
 sdata.addColumn('number', 'Positive');
 sdata.addColumn('number', 'Negative');
+sdata.addColumn('number', 'Positive subjective');
+sdata.addColumn('number', 'Negative subjective');
 sdata.addRows($r);";
 
 //var_dump($avgs);
@@ -27,18 +29,22 @@ foreach ($avgs as $key => $sentiment) {
     $sent_html .= "sdata.setValue(" . $counter . ", 0, '" . $key . "');";
     $sent_html .= "sdata.setValue(" . $counter . ", 1, " . $sentiment[0] . ");";
     $sent_html .= "sdata.setValue(" . $counter . ", 2, " . $sentiment[1] . ");";
+    if(isset($sentiment[2]))
+    $sent_html .= "sdata.setValue(" . $counter . ", 3, " . $sentiment[2] . ");";
+    if(isset($sentiment[3]))
+    $sent_html .= "sdata.setValue(" . $counter . ", 4, " . $sentiment[3] . ");";
     $counter++;
 }
 
 $sent_html .= "
     var schart = new google.visualization.LineChart(document.getElementById('if_panel_linegraph_sentiments'));
-    schart.draw(sdata, {width:1000, height:360, fontSize:9, hAxis:{slantedTextAngle:90, slantedText:true}, chartArea:{left:50,top:10,width:850,height:300}});
+    schart.draw(sdata, {width:1000, height:360, colors:['lightblue','pink','#3366cc','#dc3912'], fontSize:9, hAxis:{slantedTextAngle:90, slantedText:true}, chartArea:{left:50,top:10,width:850,height:300}});
 ";
 
 $sent_html .= ejs();
 
 $sent_html .= '
-<div class="txt_desc"><br />Date and time are in GMT (London).</div></fieldset>
+<div class="txt_desc"><br /></div></fieldset>
 ';
 
 echo $sent_html;
@@ -69,6 +75,7 @@ function sentiment_avgs() {
     global $esc;
     global $period;
 
+    // all sentiments
     $sql = "SELECT avg(s.positive) as pos, avg(s.negative) as neg, ";
     if ($period == "day") // @todo
         $sql .= "DATE_FORMAT(t.created_at,'%Y.%d.%m') datepart ";
@@ -77,16 +84,33 @@ function sentiment_avgs() {
     $sql .= "FROM " . $esc['mysql']['dataset'] . "_tweets t, ";
     $sql .= $esc['mysql']['dataset'] . "_sentiment s ";
     $sql .= sqlSubset("t.id = s.tweet_id AND ");
-    $sql .= "GROUP BY datepart";
+    $sql .= "GROUP BY datepart ORDER BY t.created_at";
 
     $rec = mysql_query($sql);
     while ($res = mysql_fetch_assoc($rec)) {
         $neg = $res['neg'];
         $pos = $res['pos'];
-        $el = array();
-        $el[] = (float) $pos;
-        $el[] = (float) abs($neg);
-        $avgs[$res['datepart']] = $el;
+        $avgs[$res['datepart']][0] = (float) $pos;
+        $avgs[$res['datepart']][1] = (float) abs($neg);
+    }
+
+    // only subjective
+    $sql = "SELECT avg(s.positive) as pos, avg(s.negative) as neg, ";
+    if ($period == "day") // @todo
+        $sql .= "DATE_FORMAT(t.created_at,'%Y.%d.%m') datepart ";
+    else
+        $sql .= "DATE_FORMAT(t.created_at,'%d. %H:00h') datepart ";
+    $sql .= "FROM " . $esc['mysql']['dataset'] . "_tweets t, ";
+    $sql .= $esc['mysql']['dataset'] . "_sentiment s ";
+    $sql .= sqlSubset("t.id = s.tweet_id AND (s.positive != 1 AND s.negative != 1) AND ");
+    $sql .= "GROUP BY datepart ORDER BY t.created_at";
+
+    $rec = mysql_query($sql);
+    while ($res = mysql_fetch_assoc($rec)) {
+        $neg = $res['neg'];
+        $pos = $res['pos'];
+        $avgs[$res['datepart']][2] = (float) $pos;
+        $avgs[$res['datepart']][3] = (float) abs($neg);
     }
     return $avgs;
 }
