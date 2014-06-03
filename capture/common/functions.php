02b292b7aef021dd3b7284287f8115a2d2b932fa
dentoir <emile@schijnheilig.org>
2014-06-03 15:25:50 +0200

    Remove verbose debugging

--- capture/common/functions.php
+++ capture/common/functions.php
@@ -1383,10 +1383,7 @@ function processtweets($tweetbucket) {
 
                                     foreach ($boxes as $box) {
                                         if (coordinatesInsideBoundingBox($tweet_lng, $tweet_lat, $box['sw_lng'], $box['sw_lat'], $box['ne_lng'], $box['ne_lat'])) {
-                                            logit(CAPTURE . ".error.log", "(debug) tweet with lng $tweet_lng and lat $tweet_lat versus (sw: " . $box['sw_lng'] . "," . $box['sw_lat'] . " ne: " . $box['ne_lng'] . "," . $box['ne_lat'] . ") matched to be inside the area");
                                             $found = true; break;
-                                        } else {
-                                            logit(CAPTURE . ".error.log", "(debug) tweet with lng $tweet_lng and lat $tweet_lat versus (sw: " . $box['sw_lng'] . "," . $box['sw_lat'] . " ne: " . $box['ne_lng'] . "," . $box['ne_lat'] . ") falls outside the area");
                                         }
                                     }
 
@@ -1443,7 +1440,6 @@ function processtweets($tweetbucket) {
                                         $boxcontains = $versus->contains($place);
                                         $overlaps = $place->overlaps($versus);
                                         if (!$contains && ($boxcontains || $overlaps)) {
-                                                logit(CAPTURE . ".error.log", "place polygon $wkt allies with geobox $boxwkt");
                                                 $found = true; break;
                                         }
 
