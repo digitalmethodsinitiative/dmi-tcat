0ab089f4f78f710734139b075f48eb116e023f81
Erik Borra <erik@erikborra.net>
2014-07-03 14:00:26 +0200

    provide full export for geo tweets

--- analysis/mod.location.php
+++ analysis/mod.location.php
@@ -31,32 +31,84 @@ $uselocalresults = false;   // @todo used as hack for experiment in first issue 
         <?php
         validate_all_variables();
 
-        $sql = "SELECT * FROM ".$esc['mysql']['dataset']."_tweets t ";
+        $header = "id,time,created_at,from_user_name,from_user_lang,text,source,location,lat,lng,from_user_follower_count,from_user_friend_count,from_user_realname,to_user_name,in_reply_to_status_id,from_user_listed,from_user_utcoffset,from_user_timezone,from_user_description,from_user_url,from_user_verified,filter_level";
+        if (isset($_GET['includeUrls']) && $_GET['includeUrls'] == 1)
+            $header .= ",urls,urls_expanded,urls_followed,domains";
+        $header .= "\n";
+
+        $sql = "SELECT * FROM " . $esc['mysql']['dataset'] . "_tweets t ";
         $where = "geo_lat != 0 AND geo_lng != 0 AND ";
-        // @todo, add sqlinterval()
         $sql .= sqlSubset($where);
-        
-        //print $sql." - <br>";
-        
+
+		//echo $sql; exit;
+
         $sqlresults = mysql_query($sql);
-        
-        $filename = get_filename_for_export("location");
+        $filename = get_filename_for_export("fullExport-location"); 
         $file = fopen($filename, "w");
-        fputs($file, chr(239) . chr(187) . chr(191) .
-              "time,created_at,from_user_name,from_user_tweetcount,from_user_followercount,from_user_lang,text,source,location,geo_lat,geo_lng\n");
-        
-        while ($res = mysql_fetch_assoc($sqlresults)) {
-            fputs($file, strtotime($res["created_at"]) . "," . $res["created_at"] . "," . $res["from_user_name"] . "," . $res["from_user_tweetcount"] . "," . $res["from_user_followercount"] . "," . $res["from_user_lang"] . "," . validate($res["text"], "tweet") . ",\"" . strip_tags(html_entity_decode($res["source"])). "\",\"" . trim(strip_tags(html_entity_decode($res["location"]))). "\",".$res['geo_lat'].",".$res['geo_lng']."\n");
+        fputs($file, chr(239) . chr(187) . chr(191));
+        fputs($file, $header);
+        if ($sqlresults) {
+            while ($data = mysql_fetch_assoc($sqlresults)) {
+                $out = '';
+                if (preg_match("/_urls/", $sql))
+                    $id = $data['tweet_id'];
+                else
+                    $id = $data['id'];
+                $out .= $id . "," .
+                        strtotime($data["created_at"]) . "," .
+                        $data["created_at"] . "," .
+                        "\"" . cleanText($data["from_user_name"]) . "\"," .
+                        "\"" . $data['from_user_lang'] . "\"," .
+                        "\"" . validate($data["text"], "tweet") . "\"," .
+                        "\"" . cleanText($data["source"]) . "\"," .
+                        "\"" . cleanTExt($data["location"]) . "\"," .
+                        $data['geo_lat'] . "," .
+                        $data['geo_lng'] . "," .
+                        (isset($data['from_user_follower_count']) ? $data['from_user_follower_count'] : "") . "," .
+                        (isset($data['from_user_friend_count']) ? $data['from_user_friend_count'] : "") . "," .
+                        (isset($data['from_user_realname']) ? "\"" . cleanText($data['from_user_realname']) . "\"" : "") . "," .
+                        (isset($data['to_user_name']) ? "\"" . cleanText($data['to_user_name']) . "\"" : "") . "," .
+                        (isset($data['in_reply_to_status_id']) ? $data['in_reply_to_status_id'] : "") . "," .
+                        (isset($data['from_user_listed']) ? $data['from_user_listed'] : "") . "," .
+                        (isset($data['from_user_utcoffset']) ? $data['from_user_utcoffset'] : "") . "," .
+                        (isset($data['from_user_timezone']) ? $data['from_user_timezone'] : "") . "," .
+                        "\"" . cleanText($data['from_user_description']) . "\"," .
+                        "\"" . cleanText($data['from_user_url']) . "\"," .
+                        $data['from_user_verified'] . "," .
+                        $data['filter_level'];
+                if (isset($_GET['includeUrls']) && $_GET['includeUrls'] == 1) {
+                    $urls = $expanded = $followed = $domain = "";
+                    $sql2 = "SELECT url, url_expanded, url_followed, domain FROM " . $esc['mysql']['dataset'] . "_urls WHERE tweet_id = " . $data['id'];
+                    $rec2 = mysql_query($sql2);
+                    if (mysql_num_rows($rec2) > 0) {
+                        $res2 = mysql_fetch_assoc($rec2);
+                        $urls = $res2['url'] . " ; ";
+                        $expanded = $res2['url_expanded'] . " ; ";
+                        $followed = $res2['url_followed'] . " ; ";
+                        $domain = $res2['domain'] . " ; ";
+                    }
+                    if (!empty($urls)) {
+                        $urls = substr($urls, 0, -3);
+                        $expanded = substr($expanded, 0, -3);
+                        $followed = substr($followed, 0, -3);
+                        $domain = substr($domain, 0, -3);
+                    }
+                    $out .= "," . $urls . "," . $expanded . "," . $followed . "," . $domain;
+                }
+                $out .= "\n";
+                fputs($file, $out);
+            }
         }
-       
+
+        function cleanText($text) {
+            return preg_replace("/[\r\t\n,]/", " ", addslashes(trim(strip_tags(html_entity_decode($text)))));
+        }
+
         fclose($file);
-        
+
         echo '<fieldset class="if_parameters">';
-
         echo '<legend>Your File</legend>';
-
         echo '<p><a href="' . str_replace("#", urlencode("#"), str_replace("\"", "%22", $filename)) . '">' . $filename . '</a></p>';
-
         echo '</fieldset>';
         ?>
 
