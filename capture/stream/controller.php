9708fa85636896658dcbdee7ac81d7e5fd3821aa
Emile den Tex <emile@digitalmethods.net>
2014-09-24 15:12:03 +0200

    * explicit unlock before passthru()

--- capture/stream/controller.php
+++ capture/stream/controller.php
@@ -147,6 +147,8 @@ foreach ($roles as $role) {
 
                 if (script_lock($role, true) === true) {
                     // restart script
+                    flock($thislockfp, LOCK_UN);
+                    fclose($thislockfp);
                     passthru(PHP_CLI . " " . BASE_FILE . "capture/stream/dmitcat_$role.php > /dev/null 2>&1 &");
                 }
             }
@@ -162,6 +164,8 @@ foreach ($roles as $role) {
                 gap_record($role, $last, time());
             }
 
+            flock($thislockfp, LOCK_UN);
+            fclose($thislockfp);
             passthru(PHP_CLI . " " . BASE_FILE . "capture/stream/dmitcat_$role.php > /dev/null 2>&1 &");
         }
     }
