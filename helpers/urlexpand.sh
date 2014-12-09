fa105bf381eab2c99cb4dd18dcf919b776c4491a
Emile den Tex <emile@digitalmethods.net>
2014-12-09 15:31:54 +0100

    Add timeout

--- helpers/urlexpand.sh
+++ helpers/urlexpand.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if ! ps aux | grep  "[p]ython /var/www/dmi-tcat/helpers/urlexpand.py"
     then
-        python /var/www/dmi-tcat/helpers/urlexpand.py
+        timeout 3500 -s SIGKILL python /var/www/dmi-tcat/helpers/urlexpand.py
 fi
