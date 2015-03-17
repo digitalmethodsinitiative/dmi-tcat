67e30ab615ba3db86191b54566dfd49e7a9b4693
Emile den Tex <emile@digitalmethods.net>
2015-03-17 15:04:07 +0100

    Use utf8mb4 collation in bipartite hashtag-host graph

--- analysis/mod.hosts_hashtags.php
+++ analysis/mod.hosts_hashtags.php
@@ -30,11 +30,20 @@ require_once './common/Gexf.class.php';
         validate_all_variables();
         $filename = get_filename_for_export("hostHashtag");
 
-        $sql = "SELECT COUNT(LOWER(h.text)) AS frequency, LOWER(h.text) AS hashtag, u.domain AS domain FROM ";
+        $collation = 'utf8_bin';
+        $is_utf8mb4 = false;
+        $sql = "SHOW FULL COLUMNS FROM " . $esc['mysql']['dataset'] . "_hashtags";
+        $sqlresults = mysql_query($sql);
+        while ($res = mysql_fetch_assoc($sqlresults)) {
+            if (array_key_exists('Collation', $res) && $res['Collation'] == ('utf8mb4_unicode_ci' || $res['Collation'] == 'utf8mb4_general_ci')) { $is_utf8mb4 = true; break; }
+        }
+        if ($is_utf8mb4) $collation = 'utf8mb4_bin';
+
+        $sql = "SELECT COUNT(LOWER(h.text COLLATE $collation)) AS frequency, LOWER(h.text COLLATE $collation) AS hashtag, u.domain COLLATE $collation AS domain FROM ";
         $sql .= $esc['mysql']['dataset'] . "_tweets t, " . $esc['mysql']['dataset'] . "_hashtags h, " . $esc['mysql']['dataset'] . "_urls u ";
         $where = "t.id = h.tweet_id AND h.tweet_id = u.tweet_id AND u.url_followed !='' AND ";
         $sql .= sqlSubset($where);
-        $sql .= " GROUP BY u.domain, LOWER(h.text) ORDER BY frequency DESC";
+        $sql .= " GROUP BY u.domain, LOWER(h.text COLLATE $collation) ORDER BY frequency DESC";
         //print $sql." - <br>";
 
         $sqlresults = mysql_query($sql);
