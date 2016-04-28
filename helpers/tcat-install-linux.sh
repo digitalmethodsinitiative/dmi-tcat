5e4831e88b5af1a40980b66cca16dcc416b1df12
Hoylen Sue <hoylen@hoylen.com>
2016-04-28 10:49:26 +1000

    Make "apt-get upgrade" run without requiring user interaction.

--- helpers/tcat-install-linux.sh
+++ helpers/tcat-install-linux.sh
@@ -879,7 +879,14 @@ if [ "$DO_UPDATE_UPGRADE" = 'y' ]; then
     echo ""
 
     apt-get update
-    apt-get -y upgrade
+
+    # Upgrade without user interaction
+    # http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+    DEBIAN_FRONTEND=noninteractive \
+	apt-get -y \
+	-o Dpkg::Options::="--force-confdef" \
+	-o Dpkg::Options::="--force-confold" \
+	upgrade
 fi
 
 #----------------------------------------------------------------
